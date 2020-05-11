7aceca8097c092279bdfe2bf9d9b67fb3f486d97
Giovanni Scotti <scottigiovanni93@gmail.com>
2020-05-11 16:38:50 +0200

    Alcune migliorie e JS

--- main.js
+++ main.js
@@ -6,21 +6,24 @@
 // cliccare sull'icona con la "classe = vocal"
 $('.vocal .fa-microphone').click(
     function(){
-     console.log('click');
-    // Aggiungere un messaggio che l'utente scrive nell'imput "id = fname-due"
-     var testo_utente = $('#fname-due').val();
-     console.log(testo_utente);
-    var new_message = $('.template .message-received').clone();
-    new_message.text(testo_utente);
-// il messaggio dovrebbe comparire sopra in verde
-    $('.main-right').append(new_message);
+    invia_messaggio();
 }
 )
 
-//Inviare il messaggio con "pulsante Invio" della tastiera
-function keyCode(event) {
-    var x = event.keyCode;
-  if (x == 13) {
-    $('.vocal .fa-microphone').click();
-  }
+// Inviare il messaggio con "pulsante Invio" della tastiera
+$('#fname-due').keypress(function(event){
+    var keycode = (event.keyCode ? event.keyCode : event.which);
+    if(keycode == '13'){
+     invia_messaggio();
+     }
+});
+function invia_messaggio(){
+     // Aggiunta di un messaggio : l’utente scrive un testo nella parte bassa e cliccando
+    var testo_utente = $('#fname-due').val();
+    console.log(testo_utente);
+    // cloniamo il div
+    var new_message = $('.template .message-received').clone();
+    new_message.text(testo_utente);
+    // aggiungiamo alla pagina il nuovo messaggio
+    $('.main-right').append(new_message);
 }
